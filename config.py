1c75e2c220e679b2d363fd57b9d77129b2874fd8
nurbek030301 <75925609+nurbek030301@users.noreply.github.com>
2020-12-22 10:59:10 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = '1407084887:AAEykKtHkiOpy4yRbOtwyi_ILfcCQG_EIOQ',
+token = '1407084887:AAEykKtHkiOpy4yRbOtwyi_ILfcCQG_EIOQ'
 
 url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
